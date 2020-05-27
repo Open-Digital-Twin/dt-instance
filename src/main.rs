4846f5ef52bd89e721cb7a5a822f57ea4487640c
Rafael Trevisan <rafa.trevi@hotmail.com>
2020-05-27 08:58:58 -0300

    Implemented Rumq crate

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,82 @@
-extern crate rumqtt;
-extern crate chrono;
-extern crate text_io;
+use tokio::stream::StreamExt;
+use tokio::sync::mpsc::{channel, Sender};
+use tokio::{task,time};
 use text_io::read;
-use rumqtt::{MqttClient,MqttOptions,QoS};
+use rumq_client::{eventloop,MqttEventLoop,MqttOptions, QoS, Request, Subscribe};
 use chrono::prelude::*;
-use std::{sync::Arc};
+use std::time::Duration;
 
-fn main() {
+#[tokio::main(basic_scheduler)]
+async fn main() {
   // TODO: Connect to DB, from parameters in .env
 
   // TODO: receive connection details as parameters.
-  let opts = MqttOptions::new("Rafael","localhost",1883);
+  let (tx,rx) = channel(10);
+  let mut mqttoptions = MqttOptions::new("ELE CONECTOU SIM", "localhost", 1883);
 
-  let (client, notifications) = MqttClient::start(opts).unwrap();
+  mqttoptions.set_keep_alive(5).set_throttle(Duration::from_secs(1));
+  let mut eloop = eventloop(mqttoptions, rx);
 
-  get_instance_topics(client);
-  listen_topics(notifications);
+    let sub_list = get_instance_topics();
+
+    task::spawn(async move{
+    instance_sub_to_topics(tx, sub_list).await;
+    time::delay_for(Duration::from_secs(3)).await;
+    
+    });
+  
+    listen_topics(&mut eloop).await;
 }
 
 
-fn get_instance_topics(mut client: rumqtt::MqttClient) {
+  fn get_instance_topics() -> Vec<String> {
   // TODO: Get array of topics from DB.
-  // for each topic, client.subscribe
+  // for each topic, client.subscribe 
   // QoS is received as main parameter.
-  let mut input : String = String::new();
-  
-  println!("Type the name of the topics you wish to subscribe to.\nType 'END' to start hearing on the chosen topics.");
-  
-  while input != String::from("END") {
-    input = read!("{}");
-    client.subscribe(&input,QoS::AtLeastOnce).unwrap();
+   println!("Type the name of the topics you wish to subscribe to.\nType 'END' to start hearing on the chosen topics.");
+
+    let mut sub_list: Vec<String> = Vec::new();
+    let mut input : String = String::new();
+
+      while input != String::from("END")
+      {
+      input = read!("{}");
+      sub_list.push(String::from(&input));
+      }
+
+      sub_list
+}
+
+
+async fn instance_sub_to_topics(mut tx: Sender<Request>, sub_list : Vec<String>) {
+
+  for i in 0 .. sub_list.len() - 1{
+  let subscription = Subscribe::new(String::from(&sub_list[i]), QoS::AtLeastOnce);
+  let _ = tx.send(Request::Subscribe(subscription)).await;
   }
 
-  // TODO: return topics
+  time::delay_for(Duration::from_secs(100)).await;
 }
 
-fn listen_topics(notifications: rumqtt::Receiver<rumqtt::Notification>) {
-  println!("Hearing topics: \n");
 
-  for notification in notifications {
-    match notification {
-      rumqtt::Notification::Publish(publish) => {
-        let payload = Arc::try_unwrap(publish.payload).unwrap();
-        let topic = publish.topic_name;
-        let message: String = String::from_utf8(payload).expect("cant convert string");
-        let time_stamp = Local::now();
+
+async fn listen_topics(eloop: &mut MqttEventLoop) {
+  
+  println!("Hearing topics: \n");
+  let mut stream = eloop.connect().await.unwrap();
+  while let Some(item) = stream.next().await 
+  {
+      match item 
+      {
+      rumq_client::Notification::Publish(publish) =>{
+      let message =String::from_utf8(publish.payload).expect("cant convert");
+      let topic = publish.topic_name;
+      let time_stamp = Local::now();
 
         handle_message(topic, message, time_stamp);
       }
-      _ => handle_message_error(notification)
+
+      _ => handle_message_error(item)
     }
   }
 }
@@ -57,7 +85,7 @@ fn handle_message(topic: String, message: String, time_stamp: chrono::DateTime<c
   println!("{} , On topic : {} : {}", time_stamp.format("%d/%m/%Y %H:%M:%S"), topic, message);
 }
 
-fn handle_message_error(notification: rumqtt::Notification) {
+fn handle_message_error(notification: rumq_client::Notification) {
   println!("{:?}",notification);
 }
 
