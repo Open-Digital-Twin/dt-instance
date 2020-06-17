5f0b2f47de3f8438e73ba6dd997952e1b9d91c38
Francisco Knebel <franciscopaivaknebel@gmail.com>
2020-06-17 06:50:00 -0300

    Instance using broker from db defined data sources.

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,227 @@
 use tokio::stream::StreamExt;
-use tokio::sync::mpsc::{channel, Sender};
+use tokio::sync::mpsc::{channel, Sender, Receiver};
 use tokio::{task,time};
-use text_io::read;
-use rumq_client::{eventloop,MqttEventLoop,MqttOptions, QoS, Request, Subscribe};
+use rumq_client::{eventloop,MqttEventLoop,MqttOptions, QoS, Request, Subscribe, Unsubscribe, PacketIdentifier};
 use chrono::prelude::*;
 
 use std::time::Duration;
 use std::env;
 
-use std::sync::Arc;
+extern crate env_logger;
 
-use uuid::Uuid;
+use log::{info, error};
 
-use cdrs::authenticators::{NoneAuthenticator};
-use cdrs::cluster::session::{Session};
-use cdrs::cluster::{ClusterTcpConfig, TcpConnectionPool};
-use cdrs::load_balancing::RoundRobin;
+#[macro_use]
+#[allow(unused_imports)]
+extern crate cdrs;
 
-use serde::{Deserialize};
+#[macro_use]
+extern crate cdrs_helpers_derive;
+use cdrs::query::*;
+use crate::cdrs::frame::TryFromRow; 
 
-
-mod models;
-use models::app::CurrentSession;
-
-pub fn init_db_session() -> Arc<CurrentSession> {
-  let db_address = env::var("DB_ADDRESS").unwrap();
-
-  let node = NodeTcpConfigBuilder::new(
-    &db_address,
-    NoneAuthenticator {}
-  ).build();
-
-  let cluster_config = ClusterTcpConfig(vec![node]);
-
-  let _session: Arc<CurrentSession> = Arc::new(
-    new_session(&cluster_config, RoundRobin::new())
-      .expect("session should be created")
-  );
-
-  _session
-}
-
-#[cfg(test)]
-#[allow(dead_code)]
-pub fn get_db_session() -> Arc<CurrentSession> {
-  let _session = init_db_session();
-
-  assert!(_session.query("USE dt;").is_ok(), "Should have set keyspace.");
-
-  _session
-}
+mod common;
+use common::db::get_db_session;
+use common::models::twin::*;
 
 #[tokio::main(basic_scheduler)]
 async fn main() {
+  env_logger::init();
+  
+  let twin = env::var("TWIN_INSTANCE").unwrap();
+  info!("Current Twin: {}", twin);
+
   let host = env::var("MQTT_BROKER_ADDRESS").unwrap();
   let port = env::var("MQTT_BROKER_PORT").unwrap().parse::<u16>().unwrap();
 
-  let db = env::var("DB_ADDRESS").unwrap();
-  let twin = env::var("TWIN_INSTANCE").unwrap();
-
-
-  // TODO: Connect to DB, from parameters in .env
-  // TODO: receive connection details as parameters.
-
-  let (tx,rx) = channel(10);
-  let mut mqttoptions = MqttOptions::new("INSTANCE", host, port);
-
+  let mut mqttoptions = MqttOptions::new(twin, host, port);
   mqttoptions.set_keep_alive(5).set_throttle(Duration::from_secs(1));
+
+  let (mut tx,rx) = channel(10);
   let mut eloop = eventloop(mqttoptions, rx);
 
-  let sub_list = get_instance_topics();
+  listen_topics(&mut eloop, &mut tx).await;
+}
 
-  task::spawn(async move{
-    instance_sub_to_topics(tx, sub_list).await;
-    time::delay_for(Duration::from_secs(3)).await;
-  });
+async fn connect_to_topics(mut tx: Sender<Request>) {
+  task::spawn(async move {
+    let twin = env::var("TWIN_INSTANCE").unwrap();
+
+    let mut subscribed_to: Vec<String> = Vec::new();
+
+    loop {
+      info!("Refreshing topics for twin {}", twin);
+
+      let topics = get_instance_topics();
+      info!("Listening to {} topics.", topics.len());
+
+      let (subscribe_list, unsubscribe_list) = get_subscribe_and_unsubscribe_lists(&subscribed_to, &topics);
+
+      if !unsubscribe_list.is_empty() {
+        for topic in unsubscribe_list.clone() {
+          match subscribed_to.binary_search(&topic) {
+            Ok(index) => { subscribed_to.remove(index); },
+            Err(e) => { error!("Error finding topic to unsubscribe: {}", e); }
+          }
+        }
   
-  listen_topics(&mut eloop).await;
-}
-
-
-fn get_instance_topics() -> Vec<String> {
-  // TODO: Get array of topics from DB.
-  // for each topic, client.subscribe 
-  // QoS is received as main parameter.
-   println!("Type the name of the topics you wish to subscribe to.\nType 'END' to start hearing on the chosen topics.");
-
-  let mut sub_list: Vec<String> = Vec::new();
-  let mut input: String = String::new();
-
-  while input != String::from("END") {
-    input = read!("{}");
-    sub_list.push(String::from(&input));
-  }
-
-  sub_list
-}
-
-
-async fn instance_sub_to_topics(mut tx: Sender<Request>, sub_list : Vec<String>) {
-  for i in 0 .. sub_list.len() - 1 {
-    let subscription = Subscribe::new(String::from(&sub_list[i]), QoS::AtLeastOnce);
-    let _ = tx.send(Request::Subscribe(subscription)).await;
-  }
-
-  time::delay_for(Duration::from_secs(100)).await;
-}
-
-async fn listen_topics(eloop: &mut MqttEventLoop) {
-  println!("Hearing topics: \n");
-  let mut stream = eloop.connect().await.unwrap();
-  while let Some(item) = stream.next().await {
-    match item {
-      rumq_client::Notification::Publish(publish) => {
-        let message =String::from_utf8(publish.payload).expect("cant convert");
-        let topic = publish.topic_name;
-        let time_stamp = Local::now();
-
-        handle_message(topic, message, time_stamp);
+        // TODO: Rumq does not support unsubscribe as of 17/06/2020.
+        // Refactor and include when possible.
+        // 
+        //   let _ = tx.send(Request::Unsubscribe(Unsubscribe {
+        //     pkid: PacketIdentifier(0),
+        //     topics: unsubscribe_list
+        //   })).await;
       }
 
-      _ => handle_message_error(item)
+      if !subscribe_list.is_empty() {
+        info!("Adding {} topics.", subscribe_list.len());
+        for topic in subscribe_list {
+          subscribed_to.push(topic.clone());
+          let subscription = Subscribe::new(topic, QoS::AtLeastOnce);
+          let _ = tx.send(Request::Subscribe(subscription)).await;
+        }
+      }
+
+      subscribed_to.sort_unstable();
+  
+      time::delay_for(Duration::from_secs(30)).await;
     }
+  });
+}
+
+fn get_subscribe_and_unsubscribe_lists(subscribed_to: &Vec<String>, current_topics: &Vec<String>) -> (Vec<String>, Vec<String>) {
+  let mut subscribe_list: Vec<String> = Vec::new();
+  let mut unsubscribe_list: Vec<String> = Vec::new();
+
+  for topic in current_topics {
+    if !subscribed_to.contains(topic) {
+      subscribe_list.push((*topic).clone());
+    }
+  }
+
+  for topic in subscribed_to {
+    if !current_topics.contains(topic) {
+      unsubscribe_list.push((*topic).clone());
+    }
+  }
+
+  (subscribe_list, unsubscribe_list)
+}
+
+fn get_twin_elements() -> Vec<Element> {
+  let session = get_db_session();
+  let twin = env::var("TWIN_INSTANCE").unwrap();
+  
+
+  let element_rows = session.query(format!("SELECT * FROM element WHERE twin = {} ALLOW FILTERING", twin))
+    .expect("Elements from twin")
+    .get_body().unwrap()
+    .into_rows().unwrap();
+  
+  let mut elements: Vec<Element> = Vec::new();
+ 
+  if !element_rows.is_empty() {
+    for row in element_rows {
+      match Element::try_from_row(row.clone()) {
+        Ok(element) => elements.push(element),
+        Err(_) => {}
+      }
+    }
+  }
+
+  info!("Found {} elements in twin.", elements.len());
+
+  return elements; 
+}
+
+fn get_sources(elements: Vec<Element>) -> Vec<Source> {
+  let session = get_db_session();
+  let mut sources: Vec<Source> = Vec::new();
+
+  let mut item_ids: Vec<String> = Vec::new();
+  for element in elements {
+    item_ids.push(element.id.to_string());
+  }
+  let element_ids = item_ids.join(",");
+
+  let source_rows = session.query(format!("SELECT * FROM source WHERE element IN ({}) ALLOW FILTERING", element_ids))
+    .expect("Sources from element")
+    .get_body().unwrap()
+    .into_rows().unwrap();
+  
+  if !source_rows.is_empty() {
+    for row in source_rows {
+      match Source::try_from_row(row.clone()) {
+        Ok(source) => sources.push(source),
+        Err(_) => {}
+      }
+    }
+  }
+
+  info!("Found {} sources in twin elements.", sources.len());
+
+  return sources;
+}
+
+fn get_instance_topics() -> Vec<String> {
+  let elements = get_twin_elements();
+  let sources = get_sources(elements);
+  
+  let mut subscription_list: Vec<String> = Vec::new();
+  for source in sources {
+    subscription_list.push(source.id.to_string());
+  }
+
+  subscription_list
+}
+
+async fn listen_topics(eloop: &mut MqttEventLoop, tx: & Sender<Request>) {  
+  loop {
+    connect_to_topics(tx.clone()).await;
+
+    let mut stream = eloop.connect().await.unwrap(); // TODO handle connection error. If broker doesn't exist, it crashes here.
+    while let Some(item) = stream.next().await {
+      match item {
+        rumq_client::Notification::Puback(ack) => {
+          info!("{:?}", ack);
+        },
+        rumq_client::Notification::Suback(ack) => {
+          info!("{:?}", ack);
+        },
+        rumq_client::Notification::Publish(publish) => {
+          let message = String::from_utf8(publish.payload).expect("Convert message payload");
+  
+          let topic = publish.topic_name;
+  
+          handle_message(topic, message);
+        },
+        rumq_client::Notification::Abort(_) => {
+          info!("Connection aborted.");
+        },
+        _ => handle_message_error(item)
+      }
+    }
+    time::delay_for(Duration::from_secs(1)).await;
   }
 }
 
-fn handle_message(topic: String, message: String, time_stamp: chrono::DateTime<chrono::Local>) {
-  println!("{} , On topic : {} : {}", time_stamp.format("%d/%m/%Y %H:%M:%S"), topic, message);
+fn handle_message(topic: String, message: String) {
+  info!("{}:: {}", topic, message);
+
+  let session = get_db_session();
+
+  let response = session.query(format!(
+    "INSERT INTO source_data (source, stamp, value, created_at) VALUES ({}, toTimestamp(now()), '{}', toTimestamp(now()) )",
+    topic, message
+  ));
+
+  match response {
+    Ok(_) => info!("Inserted data for source {}.", topic),
+    Err(_) => error!("Error inserting data for source {}.", topic)
+  }
 }
 
 fn handle_message_error(notification: rumq_client::Notification) {
-  println!("{:?}",notification);
+  error!("{:?}", notification);
 }
-
-
